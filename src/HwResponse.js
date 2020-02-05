b397545127f0337dae8577569445a19c0aa03e75
Daniel Schneider <daniel.reis.schneider@gmail.com>
2020-02-05 14:20:45 -0300

    now using isomorphic-unfetch

--- src/HwResponse.js
+++ src/HwResponse.js
@@ -4,211 +4,190 @@
  *
  */
 export default class HwResponse {
+  // Constroi um objeto HwResponse com base no conteúdo String da resposta
+  constructor(response) {
+    this.response = response
+    this.camposInvalidos = null
+  }
 
-    // Constroi um objeto HwResponse com base no conteúdo String da resposta
-   constructor(response) {
-        this.response = response;
-        this.camposInvalidos = null;
-    }
-  
-      //OK, ERRO_VALIDACAO, REQUEST_INVALIDO, RECUSADO, ERRO_INTERNO;
+    // OK, ERRO_VALIDACAO, REQUEST_INVALIDO, RECUSADO, ERRO_INTERNO;
     // Retorna se a resposta foi OK
-     isOk = () => {
-        return this.response.status === 'OK';
+    isOk = () => {
+      return this.response.status === 'OK'
     };
-  
+
     // Retorna se o request teve algum erro de validação
     isErroValidacao = () => {
-      return this.response.status === 'ERRO_VALIDACAO' || this.response.status === 'VALIDATION_ERROR';
+      return this.response.status === 'ERRO_VALIDACAO' || this.response.status === 'VALIDATION_ERROR'
     };
-  
+
     // Retorna se houve algum erro no request, como url invalida ou algo do tipo
     isErroRequest = () => {
-      return this.response.status === 'REQUEST_INVALIDO' || this.response.status === 'REQUEST_ERROR';
+      return this.response.status === 'REQUEST_INVALIDO' || this.response.status === 'REQUEST_ERROR'
     };
-  
+
     // Retorna se o request foi recusado
-     isRecusado = () => {
-        return this.response.status === 'RECUSADO';
-     };
-  
+    isRecusado = () => {
+      return this.response.status === 'RECUSADO'
+    };
+
     // Retorna se houve um erro interno no servidor
-     isErroInterno = () => {
-        return this.response.status === 'ERRO_INTERNO' || this.response.status === 'INTERNAL_ERROR';
-     };
-  
+    isErroInterno = () => {
+      return this.response.status === 'ERRO_INTERNO' || this.response.status === 'INTERNAL_ERROR'
+    };
+
     // Retorna o erro de request recusado
     getErroRecusado = () => {
-      if (this.isOk())
-      return false;
-      var length = this.response.erros.length;
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
       for (var i = 0; i < length; i++) {
-        if (this.response.erros[i].tipo === 'RECUSADO')
-        return this.response.erros[i];
+        if (this.response.erros[i].tipo === 'RECUSADO') { return this.response.erros[i] }
       }
-      return false;
+      return false
     };
-  
+
     // Retorna se tem algum erro de autenticação
-     hasErroAuth = () => {
-        if (this.isOk())
-            return false;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'AUTH')
-                return true;
-        }
-        return false;
+    hasErroAuth = () => {
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'AUTH') { return true }
+      }
+      return false
     };
-  
+
     // Retorna o erro de autenticação
-     getErroAuth = () => {
-        if (this.isOk())
-            return false;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'AUTH')
-                return this.response.erros[i];
-        }
-        return false;
+    getErroAuth = () => {
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'AUTH') { return this.response.erros[i] }
+      }
+      return false
     };
-  
+
     // Retorna se tem algum erro de request inválido (tipo REQUEST)
-     hasErroRequest = () => {
-        if (this.isOk())
-            return false;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'REQUEST')
-                return true;
-        }
-        return false;
+    hasErroRequest = () => {
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'REQUEST') { return true }
+      }
+      return false
     };
-  
+
     // Retorna o erro de request inválido
-     getErroRequest = () => {
-        if (this.isOk())
-            return false;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'REQUEST')
-                return this.response.erros[i];
-        }
-        return false;
+    getErroRequest = () => {
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'REQUEST') { return this.response.erros[i] }
+      }
+      return false
     };
-  
+
     // Retorna se tem algum erro validacão nos campos (tipo CAMPOS_INVALIDOS)
-     hasErroCamposInvalidos = () => {
-        if (this.isOk())
-            return false;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'CAMPOS_INVALIDOS')
-                return true;
-        }
-        return false;
+    hasErroCamposInvalidos = () => {
+      if (this.isOk()) { return false }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'CAMPOS_INVALIDOS') { return true }
+      }
+      return false
     };
-  
+
     // Retorna um array de strings contendo o nome dos campos que estão inválidos
-     getCamposInvalidos = () => {
-        if (this.camposInvalidos !== null)
-            return this.camposInvalidos;
-        var obj = this.getObjCamposInvalidos();
-        if (obj === null)
-            return [];
-        var retorno = [];
-        for (var property in obj.campos) {
-            if (obj.campos.hasOwnProperty(property)) {
-                retorno.push(property);
-            }
+    getCamposInvalidos = () => {
+      if (this.camposInvalidos !== null) { return this.camposInvalidos }
+      var obj = this.getObjCamposInvalidos()
+      if (obj === null) { return [] }
+      var retorno = []
+      for (var property in obj.campos) {
+        if (obj.campos.hasOwnProperty(property)) {
+          retorno.push(property)
         }
-        this.camposInvalidos = retorno;
-        return retorno;
+      }
+      this.camposInvalidos = retorno
+      return retorno
     };
-  
-    //Retorna true ou false se tiver erro no campo
-     hasErroNoCampo = (nomeCampo) => {
-        return this.getCamposInvalidos().includes(nomeCampo);
+
+    // Retorna true ou false se tiver erro no campo
+    hasErroNoCampo = (nomeCampo) => {
+      return this.getCamposInvalidos().includes(nomeCampo)
     };
-  
+
     // Retorna se tem alguma mensagem de alerta
-     hasMensagemAlerta = () => {
-        if(typeof this.response.mensagemAlerta === "undefined") return false;
-        return true;
+    hasMensagemAlerta = () => {
+      if (typeof this.response.mensagemAlerta === 'undefined') return false
+      return true
     };
-  
+
     // Retorna a mensagem de alerta
-     getMensagemAlerta = () => {
-        if(!this.hasMensagemAlerta()) return null;
-        return this.response.mensagemAlerta;
+    getMensagemAlerta = () => {
+      if (!this.hasMensagemAlerta()) return null
+      return this.response.mensagemAlerta
     };
-  
+
     // Retorna se tem algum redirect
-     hasRedirect = () => {
-        if(typeof this.response.redirectTo === "undefined") return false;
-        return true;
+    hasRedirect = () => {
+      if (typeof this.response.redirectTo === 'undefined') return false
+      return true
     };
-  
+
     // Retorna a URL do redirect
-     getRedirect = () => {
-        if(!this.hasRedirect()) return null;
-        return this.response.redirectTo;
+    getRedirect = () => {
+      if (!this.hasRedirect()) return null
+      return this.response.redirectTo
     };
-  
+
     // Retorna o objeto que representa o erro no campo passado, null se não existir erro nesse campo.
     //    Esse objeto tem a seguinte estrutura:
     /*
-     {
-     codigo:"", // Um palavra que representa o tipo do erro, pode ser basicamente: NAO_INFORMADO, VALOR_INVALIDO, MIN_SIZE_INVALIDO, MAX_SIZE_INVALIDO
-     mensagem:"" // A mensagem de erro gerada pelo servidor back-end para esse erro, essa mensagem já é sanitizada e pode ser diretamente exibida para o usuário
-     }
-     */
-     getErroCampo = (nomeCampo) => {
-        if (!this.getCamposInvalidos().includes(nomeCampo))
-            return null;
-        var obj = this.getObjCamposInvalidos();
-        return obj.campos[nomeCampo];
+    {
+    codigo:"", // Um palavra que representa o tipo do erro, pode ser basicamente: NAO_INFORMADO, VALOR_INVALIDO, MIN_SIZE_INVALIDO, MAX_SIZE_INVALIDO
+    mensagem:"" // A mensagem de erro gerada pelo servidor back-end para esse erro, essa mensagem já é sanitizada e pode ser diretamente exibida para o usuário
+    }
+    */
+    getErroCampo = (nomeCampo) => {
+      if (!this.getCamposInvalidos().includes(nomeCampo)) { return null }
+      var obj = this.getObjCamposInvalidos()
+      return obj.campos[nomeCampo]
     };
-  
+
     // Retorna o status da resposta em formato String, dar preferencia ao uso da funcao isOk para testar se foi OK
-     getStatus = () => {
-        return this.response.status;
+    getStatus = () => {
+      return this.response.status
     };
-  
+
     // Retorna o conteudo da resposta, ja parseado como um objeto
-     getConteudo = () => {
-        const conteudo = this.response.conteudo || this.response.content
-        if ( conteudo === null || conteudo === "" )
-            return {};
-        return conteudo;
+    getConteudo = () => {
+      const conteudo = this.response.conteudo || this.response.content
+      if (conteudo === null || conteudo === '') { return {} }
+      return conteudo
     };
-  
+
     // Retorna erros de request (nodejs)
     getRequestErrors = () => {
-        return this.response.errors
+      return this.response.errors
     }
-  
+
     // Retorna erros de validacao (nodejs)
     getValidationErrors = () => {
-        return this.response.fieldErrors
+      return this.response.fieldErrors
     }
-  
+
     // Retorna o conteudo cru do request
     getJson = () => this.response || {}
-  
+
     // -------------------------------------------------------------------------------------------------------
     // -------------------- Funções privadas, evitar utilizar ------------------------------------------------
-  
+
     // Retorna o objeto Erro que corresponde aos campos inválidos, função interna, não usar externamente
-     getObjCamposInvalidos = () => {
-        if (!this.hasErroCamposInvalidos())
-            return null;
-        var length = this.response.erros.length;
-        for (var i = 0; i < length; i++) {
-            if (this.response.erros[i].tipo === 'CAMPOS_INVALIDOS')
-                return this.response.erros[i];
-        }
+    getObjCamposInvalidos = () => {
+      if (!this.hasErroCamposInvalidos()) { return null }
+      var length = this.response.erros.length
+      for (var i = 0; i < length; i++) {
+        if (this.response.erros[i].tipo === 'CAMPOS_INVALIDOS') { return this.response.erros[i] }
+      }
     };
-  
-  }
-  
+}
--- src/index.js
+++ src/index.js
@@ -1,92 +1,92 @@
 import * as utils from './utils'
 import HwResponse from './HwResponse'
-import PubSub from 'pubsub-js';
+import PubSub from 'pubsub-js'
+import fetch from 'isomorphic-unfetch'
 
 export default class HWApiFetch {
-  constructor(properties={fetchProperties: {}}) {
-    HWApiFetch.pending = {};
-    HWApiFetch.properties = properties;
+  constructor(properties = {fetchProperties: {}}) {
+    HWApiFetch.pending = {}
+    HWApiFetch.properties = properties
   }
 
-  static init(properties={fetchProperties: {}}) {
-    HWApiFetch.pending = {};
-    HWApiFetch.properties = properties;
+  static init(properties = {fetchProperties: {}}) {
+    HWApiFetch.pending = {}
+    HWApiFetch.properties = properties
   }
 
-  static get( path, data={}, requestId ) {
+  static get(path, data = {}, requestId) {
     const dataKeys = Object.keys(data)
     let params = ''
     dataKeys.forEach((key, index) => {
-      if(index === 0) params += '?'
+      if (index === 0) params += '?'
       params += `${key}=${data[key]}`
-      if(index !== dataKeys.length-1) params += '&'
+      if (index !== dataKeys.length - 1) params += '&'
     })
-    return this.send(`${path}${params}`, 'GET', undefined ,requestId)
+    return this.send(`${path}${params}`, 'GET', undefined, requestId)
   }
 
-  static post( path, data, requestId ) {
+  static post(path, data, requestId) {
     return this.send(path, 'POST', data, requestId)
   }
 
-  static put( path, data, requestId ) {
+  static put(path, data, requestId) {
     return this.send(path, 'PUT', data, requestId)
   }
 
-  static patch( path, data, requestId ) {
+  static patch(path, data, requestId) {
     return this.send(path, 'PATCH', data, requestId)
   }
 
-  static delete( path, data, requestId ) {
+  static delete(path, data, requestId) {
     return this.send(path, 'DELETE', data, requestId)
   }
 
   static getHeaders() {
-    if(!HWApiFetch.properties.cookiesToHeader || !Array.isArray(HWApiFetch.properties.cookiesToHeader)) return
-    
+    if (!HWApiFetch.properties.cookiesToHeader || !Array.isArray(HWApiFetch.properties.cookiesToHeader)) return
+
     const headers = {}
     HWApiFetch.properties.cookiesToHeader.forEach(e => {
-      if(typeof e !== 'object') e = {key: e, cookie: e}
-      const cookie = utils.getCookie(e.cookie);
-      if(cookie) headers[e.key] = cookie;
-    });
+      if (typeof e !== 'object') e = {key: e, cookie: e}
+      const cookie = utils.getCookie(e.cookie)
+      if (cookie) headers[e.key] = cookie
+    })
 
     return headers
   }
 
   static contentType(method) {
-    if(HWApiFetch.properties.noContentType) return {}
-    
+    if (HWApiFetch.properties.noContentType) return {}
+
     const header = {}
-    if( method === 'PATCH' ) header['Content-Type'] = 'application/json-patch+json;charset=UTF-8'
+    if (method === 'PATCH') header['Content-Type'] = 'application/json-patch+json;charset=UTF-8'
     else header['Content-Type'] = 'application/json;charset=UTF-8'
-    return header;
+    return header
   }
 
   static send(path, method, data, requestId) {
-    if(requestId) {
-      if(HWApiFetch.pending[requestId]) return new Promise((res, rej) => rej());
-      HWApiFetch.pending[requestId] = true;
+    if (requestId) {
+      if (HWApiFetch.pending[requestId]) return Promise.reject
+      HWApiFetch.pending[requestId] = true
     }
-    if(requestId) PubSub.publish("HW-API-REQUEST-START", {requestId});
+    if (requestId) PubSub.publish('HW-API-REQUEST-START', {requestId})
     return fetch(`${HWApiFetch.properties.host}${path}`, {
       method: method,
-      body: JSON.stringify( data ),
+      body: JSON.stringify(data),
       headers: {...this.getHeaders(), ...this.contentType(method)},
       ...HWApiFetch.properties.fetchProperties
     }).then(res => {
-      if(requestId) PubSub.publish("HW-API-REQUEST-END", {requestId});
-      delete HWApiFetch.pending[requestId];
-      return res;
-    }).then( res => res.json() ).then( res => {
-      if(HWApiFetch.properties.log) console.log( method + ' > ', path, data, res )
-      if(HWApiFetch.properties.beforeReturn) {
-        HWApiFetch.properties.beforeReturn.forEach( f => {
-          if(typeof f === 'function') res = f(res)
+      if (requestId) PubSub.publish('HW-API-REQUEST-END', {requestId})
+      delete HWApiFetch.pending[requestId]
+      return res
+    }).then(res => res.json()).then(res => {
+      if (HWApiFetch.properties.log) console.log(method + ' > ', path, data, res)
+      if (HWApiFetch.properties.beforeReturn) {
+        HWApiFetch.properties.beforeReturn.forEach(f => {
+          if (typeof f === 'function') res = f(res)
         })
       }
-      if(HWApiFetch.properties.hwResponse) res = new HwResponse(res)
-      return res;
+      if (HWApiFetch.properties.hwResponse) res = new HwResponse(res)
+      return res
     })
-    
   }
 }
